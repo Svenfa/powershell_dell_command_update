9c5879c1b0f3d61019c5ccb0c4aa072eafd82285
SvenFa <github@thenextdomain.de>
2018-06-01 12:07:48 +0200

    added exitcodes and debugging logs

--- update_dell_drivers.ps1
+++ update_dell_drivers.ps1
@@ -6,9 +6,25 @@
 # Defines the parameters which are given by calling this script:
 # e.g.: .\check_previous_Versions.ps1 -software "7-Zip" -version "16.01" -uninstall "yes" -debug 1
 param (
-    [int]$debug = 0,
+    [int]$debug = 1,
     [string]$OutputFileLocation = "$env:Temp\update_dell_drivers_$(get-date -f yyyy.MM.dd-H.m).log"
 )
+
+# ---- Exit Codes ----
+# Setup-routines will exit with their own exit-codes.
+# Define some custom exit-codes for this script.
+#   11000 | Dell Command | Update software not found - exited without any action
+#   11001 | Undefined error
+#   11002 | 
+#   11003 | 
+#	11004 | 
+#	11005 | 
+#	11006 | 
+#   11010 | 
+
+
+
+
 # ---- Debugging ----
 # Enable debugging (1) or disable (0)
 # Powershelldebugging:
@@ -33,18 +49,26 @@ if ($DebugMessages -eq "1") {
 # Check if this is a Dell system:
 if (Get-WmiObject win32_SystemEnclosure -Filter: "Manufacturer LIKE 'Dell Inc.'") { $isDellSystem = $true } else { $isDellSystem = $False }
 
-# Check if 'Dell Command | Update' is installed
+# Check if 'Dell Command | Update' is installed:
 Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "*Dell Command | Update*"} | ForEach-Object -process { $isDellCommandUpdateInstalled = $true }
 
+# Check if Bitlocker is enabled on Systemdrive:
+$BLinfo = Get-Bitlockervolume -MountPoint $env:SystemDrive 
+$bitlockerStatus=$($blinfo.ProtectionStatus)
 
 
+# --------------------------------------------------------------- Functions --------------------------------------------------------------
 
+# End this script with message and errorlevel
+# call this function with "endscript errormessage errorlevel" 
+# e.g.: "endscript "The cake is a lie" 2"
+function endscript($msg, $exitcode) {
+    # Debug info:
+    if ($DebugMessages -eq "1") {Write-Host "$msg"}
+    if ($DebugMessages -eq "1") {Stop-Transcript}
+    exit $exitcode
+}
 
 
 # -------------------------- Tasks --------------------------
-
-
-$BLinfo = Get-Bitlockervolume -MountPoint $env:SystemDrive && $bitlockerStatus=$($blinfo.ProtectionStatus)
-
-
-Write-Output $bitlockerStatus
+if ($isDellSystem -eq $true) { endscript "The cake is a lie" 2 }
